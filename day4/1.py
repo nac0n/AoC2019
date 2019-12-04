6f4cb2b6a52afe3c64ce32b0c778559135827059
nac0n <lager_91@hotmail.com>
2019-12-04 23:05:40 +0100

    fixed day 4 part 1

--- day4/1.py
+++ day4/1.py
@@ -1,13 +1,37 @@
-# However, they do remember a few key facts about the password:
+from collections import Counter
 
-#     It is a six-digit number.
-#     The value is within the range given in your puzzle input.
-#     Two adjacent digits are the same (like 22 in 122345).
-#     Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+RANGE_MIN = 124075
+RANGE_MAX = 580769
 
-# Other than the range rule, the following are true:
+def validated(password_digits):
+    flag = False
+    for d in range(1, len(password_digits)):
+        if(password_digits[d] < password_digits[d-1]):
+            return False
+        else:
+            # if last
+            if(d == len(password_digits)-1):
+                if(password_digits[d] == password_digits[d-1]):
+                    return True
+                elif(flag == True):
+                    return True
+                else:
+                    return False
+            # otherwise...
+            else:
+                if(password_digits[d] > password_digits[d-1]):
+                    continue
+                elif(password_digits[d] == password_digits[d-1]):
+                    flag = True
+                    continue
+                else:
+                    return False
 
-#     111111 meets these criteria (double 11, never decreases).
-#     223450 does not meet these criteria (decreasing pair of digits 50).
-#     123789 does not meet these criteria (no double).
-
+def start():
+    counter = 0
+    for password in range(RANGE_MIN, RANGE_MAX):
+        password_digits = [int(d) for d in str(password)]
+        if(validated(password_digits)):
+            counter += 1
+    print(counter)
+start()
